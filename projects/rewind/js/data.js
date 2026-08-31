4620657a875004cfe9238fcd37e18ca97708e782
isha <aishafermin2003@gmail.com>
2026-08-31 15:33:41 -0400

    added songs

--- projects/rewind/js/data.js
+++ projects/rewind/js/data.js
@@ -33,6 +33,167 @@ const songs = [
         genre: "R&B",
         cover: "assets/covers/multiply-woodz.jpg",
         audio: "assets/audio/multiply-woodz.mp3"
+    },
+
+    {
+        id: "need-u-the-most-highvyn",
+        title: "need u the most",
+        artist: "HIGHVYN",
+        genre: "R&B",
+        cover: "assets/covers/need-u-the-most-highvyn.jpg",
+        audio: "assets/audio/need-u-the-most-highvyn.mp3"
+    },
+
+    {
+        id: "comin-through-hojean",
+        title: "comin' through",
+        artist: "Hojean",
+        genre: "R&B",
+        cover: "assets/covers/comin-through-hojean.jpg",
+        audio: "assets/audio/comin-through-hojean.mp3"
+    },
+
+    {
+        id: "9-to-5-hojean",
+        title: "9 to 5",
+        artist: "Hojean",
+        genre: "R&B",
+        cover: "assets/covers/9-to-5-hojean.jpg",
+        audio: "assets/audio/9-to-5-hojean.mp3"
+    },
+
+    {
+        id: "cigarettes-starfall",
+        title: "cigarettes",
+        artist: "starfall",
+        genre: "R&B",
+        cover: "assets/covers/cigarettes-starfall.jpg",
+        audio: "assets/audio/cigarettes-starfall.mp3"
+    },
+
+    {
+        id: "cruel-starfall",
+        title: "cruel",
+        artist: "starfall",
+        genre: "R&B",
+        cover: "assets/covers/cruel-starfall.jpg",
+        audio: "assets/audio/cruel-starfall.mp3"
+    },
+
+    {
+        id: "hype-boy-newjeans",
+        title: "Hype Boy",
+        artist: "NewJeans",
+        genre: "K-Pop",
+        cover: "assets/covers/hype-boy-newjeans.jpg",
+        audio: "assets/audio/hype-boy-newjeans.mp3"
+    },
+
+    {
+        id: "magnetic-illit",
+        title: "Magnetic",
+        artist: "ILLIT",
+        genre: "K-Pop",
+        cover: "assets/covers/magnetic-illit.jpg",
+        audio: "assets/audio/magnetic-illit.mp3"
+    },
+
+    {
+        id: "surfin-stray-kids",
+        title: "Surfin'",
+        artist: "Stray Kids",
+        genre: "K-Pop",
+        cover: "assets/covers/surfin-stray-kids.jpg",
+        audio: "assets/audio/surfin-stray-kids.mp3"
+    },
+
+    {
+        id: "just-one-day-bts",
+        title: "Just One Day",
+        artist: "BTS",
+        genre: "K-Pop",
+        cover: "assets/covers/just-one-day-bts.jpg",
+        audio: "assets/audio/just-one-day-bts.mp3"
+    },
+
+    {
+        id: "greater-than-one-ericdoa",
+        title: "greater than one",
+        artist: "VALORANT, ericdoa",
+        genre: "Electronic",
+        cover: "assets/covers/greater-than-one-ericdoa.jpg",
+        audio: "assets/audio/greater-than-one-ericdoa.mp3"
+    },
+
+    {
+        id: "like-jennie-jennie",
+        title: "like JENNIE",
+        artist: "JENNIE",
+        genre: "K-Pop",
+        cover: "assets/covers/like-jennie-jennie.jpg",
+        audio: "assets/audio/like-jennie-jennie.mp3"
+    },
+
+    {
+        id: "juice-brb",
+        title: "juice",
+        artist: "brb.",
+        genre: "R&B",
+        cover: "assets/covers/juice-brb.jpg",
+        audio: "assets/audio/juice-brb.mp3"
+    },
+
+    {
+        id: "a-mirage-imin",
+        title: "A Mirage",
+        artist: "I'MIN",
+        genre: "R&B",
+        cover: "assets/covers/a-mirage-imin.jpg",
+        audio: "assets/audio/a-mirage-imin.mp3"
+    },
+
+    {
+        id: "white-dept-jimmy-brown",
+        title: "White",
+        artist: "Dept, Jimmy Brown",
+        genre: "R&B",
+        cover: "assets/covers/white-dept-jimmy-brown.jpg",
+        audio: "assets/audio/white-dept-jimmy-brown.mp3"
+    },
+
+    {
+        id: "blame-pryme",
+        title: "blame",
+        artist: "pryme!",
+        genre: "R&B",
+        cover: "assets/covers/blame-pryme.jpg",
+        audio: "assets/audio/blame-pryme.mp3"
+    },
+
+    {
+        id: "kiss-and-tell-pryme",
+        title: "kiss and tell",
+        artist: "pryme!",
+        genre: "R&B",
+        cover: "assets/covers/kiss-and-tell-pryme.jpg",
+        audio: "assets/audio/kiss-and-tell-pryme.mp3"
+    },
+
+    {
+        id: "double-take-dhruv",
+        title: "double take",
+        artist: "dhruv",
+        genre: "R&B",
+        cover: "assets/covers/double-take-dhruv.jpg",
+        audio: "assets/audio/double-take-dhruv.mp3"
+    },
+
+    {
+        id: "seasons-joon",
+        title: "seasons",
+        artist: "joon",
+        genre: "R&B",
+        cover: "assets/covers/seasons-joon.jpg",
+        audio: "assets/audio/seasons-joon.mp3"
     }
 ];
-
